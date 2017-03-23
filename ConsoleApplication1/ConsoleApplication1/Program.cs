f42afd424ff4a8ffd06b5d9e63dfff63a367c27e
Scyrdan <claudehgarrettv@hotmail.com>
2017-03-23 02:02:28 -0500

    Small update

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Aye does this work");
+            Console.WriteLine("Heck yeah!");
         }
     }
 }
